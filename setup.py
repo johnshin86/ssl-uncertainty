64f2d872bdb1d5643425b71b273bf13c8ddf8872
John Shin <johnyoungshin@gmail.com>
2023-02-17 17:19:15 -0500

    Starting library.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='',
+      version='0.1',
+      description='SSL uncertainty methods.',
+      author='John Young Shin',
+      author_email='johnyoungshin@gmail.com',
+      packages=['ssl_uncertainty'],
+     )
